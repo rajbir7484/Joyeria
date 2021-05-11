29b4071ef84fd4462091e80226e44ea54b856e26
64226 <64226@LAPTOP-ECIJ602J>
2021-05-12 08:42:15 +1200

    admin

--- Joyeria/Migrations/20210507031525_aaaaaaa.Designer.cs
+++ Joyeria/Migrations/20210507031525_aaaaaaa.Designer.cs
@@ -4,14 +4,16 @@ using Joyeria.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Joyeria.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210507031525_aaaaaaa")]
+    partial class aaaaaaa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Joyeria/Migrations/20210507031525_aaaaaaa.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Joyeria.Migrations
+{
+    public partial class aaaaaaa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
